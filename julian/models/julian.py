9bfc8b443cfa961b8bcdb47ab5aee33eaacf45bf
LIvingston <la@todoo.co>
2020-02-21 10:47:02 -0500

    Revert "quitar el comentareo"
    This reverts commit ea348c047f45a75ec23cf92de8438c3ab9c59228.

--- julian/models/julian.py
+++ julian/models/julian.py
@@ -3,11 +3,9 @@ from odoo import fields, models, api
 class mrpjulian(models.Model):
 
     _inherit = 'mrp.workcenter'
-    _name = 'produccion'
     _description = 'cambios en crentros de trabajo'
 
     costo_1 = fields.Float(string="CIF")
     analityc_count = fields.Many2one('account.analytic.account',string="Analytic Account")
     costo_2 = fields.Float(string="Mano de obra")
     analityc_count2 = fields.Many2one('account.analytic.account',string="Analytic Account")
-    
