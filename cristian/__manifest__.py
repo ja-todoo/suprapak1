3680392508e4d239d3b9ea31261eaf5eb899799e
LIvingston <la@todoo.co>
2020-02-26 07:51:55 -0500

    desarrollos contrato

---
+++ cristian/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Desarrollo mrp',
+    'version': '13',
+    'author': "Livingston Arias Narváez",
+    'website': "www.todoo.com",
+    'category': '',
+    'depends': ['hr.employee','hr.contract'],
+    'data': [ 'views/cristian_view.xml',
+    ],
+    'installable': True
+}
---
+++ cristian/models/cristian.py
@@ -0,0 +1,18 @@
+from odoo import fields, models, api
+
+class ContractCristian(models.Model):
+
+    _inherit = 'hr.contract'
+    _description = 'Campos nuevos para el contrato'
+
+    ARL = fields.Many2one('res.partner')
+
+    EPS = fields.Many2one('res.partner')
+
+    Pension = fields.Many2one('res.partner',string="Pensión")
+
+    Cesantias = fields.Many2one('res.partner')
+
+    Caja = fields.Many2one('res.partner', string="Caja de compensación familiar")
+
+    
