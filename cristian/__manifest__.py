b9d42f5cf6fb7d55197f91bcb904269eccc76547
LIvingston <la@todoo.co>
2020-02-26 10:07:07 -0500

    cambio dependecias

--- cristian/__manifest__.py
+++ cristian/__manifest__.py
@@ -5,7 +5,7 @@
     'author': "Livingston Arias Narváez",
     'website': "www.todoo.com",
     'category': '',
-    'depends': ['hr.employee'],
+    'depends': ['hr'],
     'data': [ 'views/cristian_view.xml',
     ],
     'installable': True
