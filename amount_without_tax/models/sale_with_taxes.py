50fff54e10a2121d8d62313a955d43d20affedbe
LIvingston <la@todoo.co>
2020-02-27 12:37:42 -0500

    cambio

--- amount_without_tax/models/sale_with_taxes.py
+++ amount_without_tax/models/sale_with_taxes.py
@@ -1,12 +1,12 @@
 from odoo import models,fields,api
 
 class SaleTaxes(models.Model):
-    _inherit = 'sale.order'
+    _inherit = 'sale.order.line'
     _description = 'amount sum tax'
 
     def calculo_amount(self):
         if self.price_tax:
             self.price = self.price_subtotal + self.price_tax
 
-    price1=fields.Float('Subtotal',compute='calculo_amount')
+    price1 = fields.Float('Subtotal',compute='calculo_amount')
 
