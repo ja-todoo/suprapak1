b783153e0bb5b377171f482387638d9624f1e873
LIvingston <la@todoo.co>
2020-02-27 12:21:43 -0500

    .

--- amount_without_tax/models/sale_with_taxes.py
+++ amount_without_tax/models/sale_with_taxes.py
@@ -4,8 +4,9 @@ class SaleTaxes(models.Model):
     _inherit = 'sale.order'
     _description = 'amount sum tax'
 
-    price1 = fields.Float('Subtotal',compute='calculo_amount')
+    price1 = fields.Float(compute='calculo_amount')
 
     def calculo_amount(self):
         if self.price_tax:
             self.price = self.price_subtotal + self.price_tax
+
