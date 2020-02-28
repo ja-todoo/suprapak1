b71ee5145fc6c4fa7c81b9213674c41966d7374e
LIvingston <la@todoo.co>
2020-02-28 10:11:01 -0500

    cambio

--- amount_without_tax/models/sale_with_taxes.py
+++ amount_without_tax/models/sale_with_taxes.py
@@ -4,11 +4,10 @@ class SaleTaxes(models.Model):
     _inherit = 'Sales Order Line'
     _description = 'amount sum tax'
 
-    precio = float(self.price_subtotal)
-
     def calculo_amount(self):
+    
         if self.price_tax:
-            self.price = self.precio + self.price_tax
+            self.price = float(self.price_subtotal) + self.price_tax
 
-    x_studio_subtotal_with_tax = fields.Float('Subtotal',compute='calculo_amount')
+    subtotal_with_tax = fields.Float('Subtotal',compute='calculo_amount')
 
