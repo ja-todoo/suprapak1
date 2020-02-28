f172a917f2a96608b0bafa2c4df8447580eeb553
LIvingston <la@todoo.co>
2020-02-28 15:33:27 -0500

    cambio

--- amount_without_tax/__manifest__.py
+++ amount_without_tax/__manifest__.py
@@ -5,7 +5,8 @@
     'author': 'Livingston Arias Narváez',
     'summary': 'Sales internal with tax',
     'depends': ['sale'],
-    'data': ['views/sale_with_taxes_view.xml', ],
+    'data': ['views/sale_with_taxes_view.xml',
+            'views/invoice.xml',],
 
     'installable': True,
     'auto_install': False,
